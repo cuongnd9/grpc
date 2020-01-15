a819ed94644fe4c4f518fe2102fc185ae140d127
Nguyen Duy Cuong <cuongw.me@gmail.com>
2020-01-15 23:57:13 +0700

    Add basic Node gRPC

---
+++ node/index.js
@@ -0,0 +1,30 @@
+const grpc = require('grpc');
+
+// mock data.
+const cats = [
+  {
+    id: '80f6563e-a26e-4270-afc2-af09986280d1',
+    name: 'DOg',
+    color: 'yellow',
+    image: 'https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+  },
+  {
+    id: '80f6563e-a26e-4270-afc2-af09986280c2',
+    name: 'Lily',
+    color: 'black',
+    image: 'https://images.unsplash.com/photo-1503431128871-cd250803fa41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+  }
+]
+
+// load proto file.
+const catsProto = grpc.load('cats.proto');
+// create new gRPC server.
+const server = new grpc.Server();
+server.addService(catsProto.CatService.service, {
+  list: (_, callback) => {
+    callback(null, cats);
+  }
+})
+server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
+console.log('Server is running at http:127.0.0.1:50051');
+server.start();
